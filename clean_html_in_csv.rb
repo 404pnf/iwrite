dba940e638c4fca1bce39e56b918ba7ff295de62
404pnf <404pnf@exmaple.org>
2013-07-18 09:16:12 +0800

    只特定字段才html->text过滤，否则可能引入错误，比如哪些不需要过滤的字段有大于小于号什么的

--- clean_html_in_csv.rb
+++ clean_html_in_csv.rb
@@ -6,10 +6,11 @@ c = CSV.read('itest-xiezuo.csv', headers: false,)
 
 # csv header
 # ["标题和要求", "范文", nil, "分析", "考试类型", "附件"]
-
-c_clean = c.map do |record|
-  cleanup = record.map { |i| HtmlMassage.text i unless i.nil?}
-  cleanup.to_csv(force_quotes: true)
+c_clean = c.map do |title, article, unknown, comment, type, attach|
+  title_clean = HtmlMassage.text title unless title.nil?
+  article_clean = HtmlMassage.text article unless article.nil?
+  comment_clean = HtmlMassage.text comment unless comment.nil?
+ [title_clean, article_clean, unknown, comment_clean, type, attach].to_csv(force_quotes: true) 
 end
 
 File.write('itest-xiezuo-cleanup.csv', c_clean.join)

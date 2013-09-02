17435922059fdc6c7e1f3ebabd0f49a94535de11
404pnf <404pnf@exmaple.org>
2013-09-02 17:47:33 +0800

    重构。简化代码

--- clean_html_in_csv.rb
+++ clean_html_in_csv.rb
@@ -2,17 +2,24 @@ require 'csv'
 require 'fileutils'
 require 'html_massage'
 
-c = CSV.read('itest-xiezuo.csv', headers: false,)
+# USAGE: ruby -w script.rb inputfile
 
 # csv header
 # ["标题和要求", "范文", nil, "分析", "考试类型", "附件"]
-c_clean = c.map do |title, article, unknown, comment, type, attach|
-  title_clean = HtmlMassage.text title unless title.nil?
-  article_clean = HtmlMassage.text article unless article.nil?
-  comment_clean = HtmlMassage.text comment unless comment.nil?
- [title_clean, article_clean, unknown, comment_clean, type, attach].to_csv(force_quotes: true) 
+
+def html2text(*arr)
+  arr.map { |str| HtmlMassage.text str unless str.nil? }
 end
 
-File.write('itest-xiezuo-cleanup.csv', c_clean.join)
+def cleanup_html inputfile
+	c = CSV.read(inputfile, headers: false,)
+	c.map do |title, article, unknown, comment, type, attach|	
+		title_clean, article_clean, comment_clean = html2text(title, article, comment)
+	  [title_clean, article_clean, unknown, comment_clean, type, attach].to_csv(force_quotes: true) 
+	end
+end
 
-  
+inputfile = ARGV[0] || 'itest-xiezuo.csv'
+c_clean = cleanup_html(inputfile)
+p "生成了itest-xiezuo-cleanup.csv"
+File.write('itest-xiezuo-cleanup.csv', c_clean.join)
